8cb178fd9350096299cef1e702ac2a371ece79d4
Yang Liu <liuyang22@iscas.ac.cn>
2023-01-09 14:07:24 +0800

    Remove unused code

--- src/reg.h
+++ src/reg.h
@@ -7,14 +7,3 @@ enum gp_reg_type_t {
     t3, t4, t5, t6,
     num_gp_regs,
 };
-
-typedef union {
-    struct { i64 v; } x;
-    struct { u64 v; } xu;
-    struct { i32 v; u8 _pad[4]; } w;
-    struct { u32 v; u8 _pad[4]; } wu;
-    struct { i16 v; u8 _pad[6]; } h;
-    struct { u16 v; u8 _pad[6]; } hu;
-    struct { i8  v; u8 _pad[7]; } b;
-    struct { u8  v; u8 _pad[7]; } bu;
-} gp_reg_t;
