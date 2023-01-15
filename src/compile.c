8fc461b6e77b9774875944eb72032064ef29bc71
Yang Liu <liuyang22@iscas.ac.cn>
2023-01-15 14:08:29 +0800

    Simplify

--- src/compile.c
+++ src/compile.c
@@ -1,16 +1,7 @@
 #include "rvemu.h"
 
 u8 *machine_compile(machine_t *m, str_t source) {
-    static int fd = 0;
     static char name[] = "/tmp/rvemuXXXXXX";
-    if (!fd) {
-        fd = mkstemp(name);
-    } else {
-        close(fd);
-        fd = open(name, O_RDWR | O_TRUNC);
-    }
-    int ret = write(fd, source, str_len(source));
-    if (ret == -1) fatal(strerror(errno));
 
     // using `clang` to compile it to an ELF file.
     DECLEAR_STATIC_STR(clang_str);
@@ -20,12 +11,12 @@ u8 *machine_compile(machine_t *m, str_t source) {
         "-nostdlib "
         "-xc -c -o ");
     clang_str = str_append(clang_str, name);
-    clang_str = str_append(clang_str, ".obj ");
-    clang_str = str_append(clang_str, name);
+    clang_str = str_append(clang_str, ".obj - ");
 
     FILE *f;
-    f = popen(clang_str, "r");
+    f = popen(clang_str, "w");
     if (f == NULL) fatal("cannot compile program");
+    fwrite(source, 1, str_len(source), f);
     pclose(f);
 
     // using `objcopy` to turn it into a binary
@@ -60,7 +51,7 @@ u8 *machine_compile(machine_t *m, str_t source) {
         binbuf = malloc(fsize);
     }
 
-    ret = fread(binbuf, 1, fsize, f);
+    size_t ret = fread(binbuf, 1, fsize, f);
     assert(ret == fsize);
     fclose(f);
 
